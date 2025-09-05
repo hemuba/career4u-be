70e7ca6e466ed8a29997e35bc80f6ebc982638a8
hemuba <125600353+hemuba@users.noreply.github.com>
2025-09-06 01:35:45 +0300

    Volatilita' su EUNL e NQSE pt.1

--- src/test.py
+++ src/test.py
@@ -14,8 +14,13 @@ im_dsn = os.getenv("db_dsn")
 logging.config.fileConfig("logging.ini")
 logger = logging.getLogger(__name__)
 
-sql = "SELECT TICKER, DATA, CLOSE_PRICE FROM ETF_HISTORY WHERE TICKER IN (SELECT TICKER FROM CURRENT_ETF) AND DATA >= '01-JAN-2020' ORDER BY DATA"
-ticker_e_prices = {}
+sql = "SELECT TICKER, DATA, CLOSE_PRICE FROM ETF_HISTORY WHERE TICKER IN (SELECT TICKER FROM CURRENT_ETF) AND DATA >= '01-JAN-2019' ORDER BY DATA"
+
+etfs = {}
+close_prices_eunl = []
+rendimenti_eunl = []
+close_prices_nqse = []
+rendimenti_nqse = []
 with odb.connect(user=im_user, password=im_pwd, dsn=im_dsn) as connection:
     with connection.cursor() as cursor:
         try:
@@ -28,58 +33,53 @@ with odb.connect(user=im_user, password=im_pwd, dsn=im_dsn) as connection:
                 ticker = table[i][0]
                 data = table[i][1]
                 close_price = table[i][2]
-
-                if ticker in ticker_e_prices:
-                    ticker_e_prices[ticker].append((data, close_price))
-                else:
-                    ticker_e_prices[ticker] = [(data, close_price)]
-            
-            best_prices = {}
-            etf_dominance = {}
-            totale_per_data = {}
-            try:
-                for ticker, data_e_prezzo in ticker_e_prices.items():
-
-                    max_data = None
-                    max_price = 0.0
-
-                    for data, prezzo in data_e_prezzo:
-                        if prezzo > max_price:
-                            max_price = prezzo
-                            max_data = data
-                    best_prices[ticker] = (max_data, max_price)
-            
-                    
-                    
-                for ticker, lista in ticker_e_prices.items():
-                    for data, prezzo in lista:
-                        if data not in totale_per_data:
-                            totale_per_data[data] = 0
-                        totale_per_data[data] += prezzo
-                    
-                    
-                for ticker, (data, price) in best_prices.items():
-                        d_max = data
-                        p_max = price
-                        denominatore = totale_per_data.get(d_max)
-                        if bool(denominatore) == False:
-                            logger.warning(f"Nessun totale per data {d_max}")
-                            continue
-                        perc = p_max / denominatore * 100
-                        etf_dominance[ticker] = (d_max, perc)
-      
-                    
-                sorted_etf_dominance = dict(sorted(etf_dominance.items(), key= lambda x:x[1][1], reverse=True))
-                print(sorted_etf_dominance)
-                for k, v in sorted_etf_dominance.items():
-                    data = v[0].strftime("%d-%m-%Y")
-                    peso = round(v[1], 2)
-                    print(data, k, peso)
-
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                logger.error(f"An error occurred: {e}")
                 
+                if ticker in etfs:
+                    etfs[ticker].append((data, close_price))
+                else:
+                    etfs[ticker] = [(data, close_price)]
+            
+            for ticker, tupla in etfs.items():
+                if ticker == "EUNL.DE":
+                    for d, p in tupla:
+                        close_prices_eunl.append(p)
+                elif ticker == "NQSE.DE":
+                    for d, p in tupla:
+                        close_prices_nqse.append(p)
+
+            for i in range(1, len(close_prices_eunl)):
+                rendimenti_eunl.append((close_prices_eunl[i] / close_prices_eunl[i-1] - 1 ) * 100)
+                
+            for i in range(1, len(close_prices_nqse)):
+                rendimenti_nqse.append((close_prices_nqse[i] / close_prices_nqse[i-1] - 1) * 100)
+                
+                
+            if len(rendimenti_eunl) - len(rendimenti_nqse) != 0:
+                raise RuntimeError(f"Cannot compare entities. Len entities is not the same.")
+            
+            if len(rendimenti_eunl) != len(close_prices_eunl) -1:
+                raise RuntimeError(f"Rendimenti_eunl is: {len(rendimenti_eunl)}, should be {len(close_prices_eunl) -1}")
+            
+            den_eunl = len(rendimenti_eunl)
+            MA_eunl = sum(rendimenti_eunl) / den_eunl
+            
+            den_nqse = len(rendimenti_nqse)
+            MA_nqse = sum(rendimenti_nqse) / den_nqse
+            
+            scostamento_quadratico_eunl = [(r -  MA_eunl)**2 for r in rendimenti_eunl]
+            volatilita_eunl_daily = (sum(scostamento_quadratico_eunl) / den_eunl) ** 0.5
+            
+            scostamento_quadratico_nqse = [(r- MA_nqse) **2 for r in rendimenti_nqse]
+            volatilita_nqse_daily = (sum(scostamento_quadratico_nqse) / den_nqse) ** 0.5
+            
+            
+            volalita_annualizzata_eunl = volatilita_eunl_daily * (252**0.5)
+            print(volalita_annualizzata_eunl)
+            
+            volalita_annualizzata_nqse = volatilita_nqse_daily * (252**0.5)
+            
+            print(f"Vol EUNL: {volalita_annualizzata_eunl}")
+            print(f"Vol NQSE: {volalita_annualizzata_nqse}")
             
         except Exception as e:
             print(f"Errore durante l'esecuzione della query: {sql} - {e}")
