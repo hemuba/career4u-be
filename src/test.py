ea9ba0110d4731e05e083aad8f5eeae427a3bf69
hemuba <125600353+hemuba@users.noreply.github.com>
2025-09-06 18:53:32 +0300

    Returns on my ETFs from JEDI first date

--- src/test.py
+++ src/test.py
@@ -14,13 +14,12 @@ im_dsn = os.getenv("db_dsn")
 logging.config.fileConfig("logging.ini")
 logger = logging.getLogger(__name__)
 
-sql = "SELECT TICKER, DATA, CLOSE_PRICE FROM ETF_HISTORY WHERE TICKER IN (SELECT TICKER FROM CURRENT_ETF) AND DATA >= '01-JAN-2019' ORDER BY DATA"
+sql = "SELECT TICKER, DATA, CLOSE_PRICE FROM ETF_HISTORY WHERE TICKER IN (SELECT TICKER FROM CURRENT_ETF) AND DATA >= '24-JUN-2022' ORDER BY DATA"
 
 etfs = {}
-close_prices_eunl = []
-rendimenti_eunl = []
-close_prices_nqse = []
-rendimenti_nqse = []
+etfs_and_prices= {}
+etf_and_rendimenti = {}
+rendimento_perc_etf = {}
 with odb.connect(user=im_user, password=im_pwd, dsn=im_dsn) as connection:
     with connection.cursor() as cursor:
         try:
@@ -33,54 +32,50 @@ with odb.connect(user=im_user, password=im_pwd, dsn=im_dsn) as connection:
                 ticker = table[i][0]
                 data = table[i][1]
                 close_price = table[i][2]
-                
+            
                 if ticker in etfs:
+                    
                     etfs[ticker].append((data, close_price))
                 else:
                     etfs[ticker] = [(data, close_price)]
-            
-            for ticker, tupla in etfs.items():
-                if ticker == "EUNL.DE":
-                    for d, p in tupla:
-                        close_prices_eunl.append(p)
-                elif ticker == "NQSE.DE":
-                    for d, p in tupla:
-                        close_prices_nqse.append(p)
 
-            for i in range(1, len(close_prices_eunl)):
-                rendimenti_eunl.append((close_prices_eunl[i] / close_prices_eunl[i-1] - 1 ) * 100)
+            for ticker, lista in etfs.items():
+                for d, p in lista:
+                    if ticker in etfs_and_prices:
+                        etfs_and_prices[ticker].append(p)
+                    else:
+                        etfs_and_prices[ticker] = [p]
+           
                 
-            for i in range(1, len(close_prices_nqse)):
-                rendimenti_nqse.append((close_prices_nqse[i] / close_prices_nqse[i-1] - 1) * 100)
+            for ticker, prices in etfs_and_prices.items():
+                rendimento = []
+                for i in range(1, len(prices)):
+                    r = prices[i] / prices[i-1] -1
+                    rendimento.append(r)
+                etf_and_rendimenti[ticker] = rendimento
                 
+            for ticker, lista in etfs.items():
                 
-            if len(rendimenti_eunl) - len(rendimenti_nqse) != 0:
-                raise RuntimeError(f"Cannot compare entities. Len entities is not the same.")
+                lista.sort(key= lambda x: x[0])
+                
+                first_price = lista[0][1]
+                last_price = lista[-1][1]
+                
+                rendimento_perc_etf[ticker] = (last_price / first_price - 1) * 100
+                
+            for k, v in rendimento_perc_etf.items():
+                print(k, round(v,2))
+                
+            rendimenti_effettivi =sum(rendimento_perc_etf.values())
             
-            if len(rendimenti_eunl) != len(close_prices_eunl) -1:
-                raise RuntimeError(f"Rendimenti_eunl is: {len(rendimenti_eunl)}, should be {len(close_prices_eunl) -1}")
+            den = len(rendimento_perc_etf.values())
             
-            den_eunl = len(rendimenti_eunl)
-            MA_eunl = sum(rendimenti_eunl) / den_eunl
+            ma = rendimenti_effettivi / den
             
-            den_nqse = len(rendimenti_nqse)
-            MA_nqse = sum(rendimenti_nqse) / den_nqse
-            
-            scostamento_quadratico_eunl = [(r -  MA_eunl)**2 for r in rendimenti_eunl]
-            volatilita_eunl_daily = (sum(scostamento_quadratico_eunl) / den_eunl) ** 0.5
-            
-            scostamento_quadratico_nqse = [(r- MA_nqse) **2 for r in rendimenti_nqse]
-            volatilita_nqse_daily = (sum(scostamento_quadratico_nqse) / den_nqse) ** 0.5
-            
-            
-            volalita_annualizzata_eunl = volatilita_eunl_daily * (252**0.5)
-            print(volalita_annualizzata_eunl)
-            
-            volalita_annualizzata_nqse = volatilita_nqse_daily * (252**0.5)
-            
-            print(f"Vol EUNL: {volalita_annualizzata_eunl}")
-            print(f"Vol NQSE: {volalita_annualizzata_nqse}")
-            
+            print(f"Il portafogli {" - ".join(etf_and_rendimenti.keys())} ha una % di guadagno del: {round(ma,2)}% dal {list(etfs.values())[0][0][0].year} ad oggi {datetime.now().year}")
+                
+                   
+
         except Exception as e:
             print(f"Errore durante l'esecuzione della query: {sql} - {e}")
             logger.error(f"Errore durante l'esecuzione della query: {sql} - {e}")
