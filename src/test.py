2ef20ea1f00a62aa529ebe572c786c19bbd9b2e4
hemuba <125600353+hemuba@users.noreply.github.com>
2025-09-06 20:11:52 +0300

    Max DD EUNL.DE

--- src/test.py
+++ src/test.py
@@ -14,12 +14,9 @@ im_dsn = os.getenv("db_dsn")
 logging.config.fileConfig("logging.ini")
 logger = logging.getLogger(__name__)
 
-sql = "SELECT TICKER, DATA, CLOSE_PRICE FROM ETF_HISTORY WHERE TICKER IN (SELECT TICKER FROM CURRENT_ETF) AND DATA >= '24-JUN-2022' ORDER BY DATA"
+sql = "SELECT TICKER, DATA, CLOSE_PRICE FROM ETF_HISTORY WHERE TICKER IN (SELECT TICKER FROM CURRENT_ETF) AND DATA >= '01-JAN-2019' ORDER BY DATA"
 
-etfs = {}
-etfs_and_prices= {}
-etf_and_rendimenti = {}
-rendimento_perc_etf = {}
+eunl_prices = {}
 with odb.connect(user=im_user, password=im_pwd, dsn=im_dsn) as connection:
     with connection.cursor() as cursor:
         try:
@@ -32,50 +29,45 @@ with odb.connect(user=im_user, password=im_pwd, dsn=im_dsn) as connection:
                 ticker = table[i][0]
                 data = table[i][1]
                 close_price = table[i][2]
+                
             
-                if ticker in etfs:
-                    
-                    etfs[ticker].append((data, close_price))
-                else:
-                    etfs[ticker] = [(data, close_price)]
-
-            for ticker, lista in etfs.items():
-                for d, p in lista:
-                    if ticker in etfs_and_prices:
-                        etfs_and_prices[ticker].append(p)
+                if ticker == "EUNL.DE":
+                    if ticker in eunl_prices:
+                        eunl_prices[ticker].append(close_price)
                     else:
-                        etfs_and_prices[ticker] = [p]
-           
-                
-            for ticker, prices in etfs_and_prices.items():
-                rendimento = []
-                for i in range(1, len(prices)):
-                    r = prices[i] / prices[i-1] -1
-                    rendimento.append(r)
-                etf_and_rendimenti[ticker] = rendimento
-                
-            for ticker, lista in etfs.items():
-                
-                lista.sort(key= lambda x: x[0])
-                
-                first_price = lista[0][1]
-                last_price = lista[-1][1]
-                
-                rendimento_perc_etf[ticker] = (last_price / first_price - 1) * 100
-                
-            for k, v in rendimento_perc_etf.items():
-                print(k, round(v,2))
-                
-            rendimenti_effettivi =sum(rendimento_perc_etf.values())
+                        eunl_prices[ticker] = [close_price]
+                        
+            drop_t = []
+            for ticker, lista in eunl_prices.items():
+                max_val = lista[0] 
+                for i in range(len(lista)):
+                    
+                    current_val = lista[i] 
+                    
+                    if current_val > max_val:
+                        max_val = current_val
+                    drop_t.append((current_val - max_val) / max_val * 100)
+               
+            if drop_t:     
+                if drop_t[0] != 0 or len(drop_t) != len(eunl_prices["EUNL.DE"]):
+                    raise RuntimeError("Error: check your logic, there is an issue calculating the DrawDown")
             
-            den = len(rendimento_perc_etf.values())
-            
-            ma = rendimenti_effettivi / den
-            
-            print(f"Il portafogli {" - ".join(etf_and_rendimenti.keys())} ha una % di guadagno del: {round(ma,2)}% dal {list(etfs.values())[0][0][0].year} ad oggi {datetime.now().year}")
+                if any(n > 0 for n in drop_t):
+                    raise RuntimeError("Error: check your logic, there is an issue calculating the DrawDown")
+            else:
+                raise RuntimeError("Error: check your logic, there is an issue calculating the DrawDown")
                 
-                   
+                
+            
+            max_dd = min(drop_t)
+    
+            
+            print(f"Max DD for EUNL.DE: {round(max_dd, 2)}%")
+                    
+                               
 
+        
+            
         except Exception as e:
             print(f"Errore durante l'esecuzione della query: {sql} - {e}")
             logger.error(f"Errore durante l'esecuzione della query: {sql} - {e}")
