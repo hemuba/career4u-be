a6d0eff6fbe12f5194c144340903c941c1ccdf93
hemuba <125600353+hemuba@users.noreply.github.com>
2025-09-06 20:35:53 +0300

    DD EUNL.DE and NQSE.DE

--- src/test.py
+++ src/test.py
@@ -16,7 +16,8 @@ logger = logging.getLogger(__name__)
 
 sql = "SELECT TICKER, DATA, CLOSE_PRICE FROM ETF_HISTORY WHERE TICKER IN (SELECT TICKER FROM CURRENT_ETF) AND DATA >= '01-JAN-2019' ORDER BY DATA"
 
-eunl_prices = {}
+eunl_prices = []
+nqse_prices = []
 with odb.connect(user=im_user, password=im_pwd, dsn=im_dsn) as connection:
     with connection.cursor() as cursor:
         try:
@@ -32,37 +33,54 @@ with odb.connect(user=im_user, password=im_pwd, dsn=im_dsn) as connection:
                 
             
                 if ticker == "EUNL.DE":
-                    if ticker in eunl_prices:
-                        eunl_prices[ticker].append(close_price)
-                    else:
-                        eunl_prices[ticker] = [close_price]
+                    eunl_prices.append(close_price)
+                elif ticker == "NQSE.DE":
+                    nqse_prices.append(close_price)
                         
-            drop_t = []
-            for ticker, lista in eunl_prices.items():
-                max_val = lista[0] 
-                for i in range(len(lista)):
+            drop_t_eunl = []
+            eunl_max_val = eunl_prices[0] 
+            for i in range(len(eunl_prices)):
                     
-                    current_val = lista[i] 
+                    current_val = eunl_prices[i] 
                     
-                    if current_val > max_val:
-                        max_val = current_val
-                    drop_t.append((current_val - max_val) / max_val * 100)
+                    if current_val > eunl_max_val:
+                        eunl_max_val = current_val
+                    drop_t_eunl.append((current_val - eunl_max_val) / eunl_max_val * 100)
                
-            if drop_t:     
-                if drop_t[0] != 0 or len(drop_t) != len(eunl_prices["EUNL.DE"]):
+            if drop_t_eunl:     
+                if drop_t_eunl[0] != 0 or len(drop_t_eunl) != len(eunl_prices):
                     raise RuntimeError("Error: check your logic, there is an issue calculating the DrawDown")
             
-                if any(n > 0 for n in drop_t):
+                if any(n > 0 for n in drop_t_eunl):
+                    raise RuntimeError("Error: check your logic, there is an issue calculating the DrawDown")
+            else:
+                raise RuntimeError("Error: check your logic, there is an issue calculating the DrawDown")
+            
+            drop_t_nqse = []
+            nqse_max_val = nqse_prices[0] 
+            for i in range(len(nqse_prices)):
+                    
+                    current_val = nqse_prices[i] 
+                    
+                    if current_val > nqse_max_val:
+                        nqse_max_val = current_val
+                    drop_t_nqse.append((current_val - nqse_max_val) / nqse_max_val * 100)
+               
+            if drop_t_nqse:     
+                if drop_t_nqse[0] != 0 or len(drop_t_nqse) != len(nqse_prices):
+                    raise RuntimeError("Error: check your logic, there is an issue calculating the DrawDown")
+            
+                if any(n > 0 for n in drop_t_nqse):
                     raise RuntimeError("Error: check your logic, there is an issue calculating the DrawDown")
             else:
                 raise RuntimeError("Error: check your logic, there is an issue calculating the DrawDown")
                 
                 
             
-            max_dd = min(drop_t)
-    
+            max_dd_eunl = min(drop_t_eunl)
+            max_dd_nqse = min(drop_t_nqse)
             
-            print(f"Max DD for EUNL.DE: {round(max_dd, 2)}%")
+            print(f"Max DD for EUNL.DE: {round(max_dd_eunl, 2)}%\nMax DD for NQSE.DE: {round(max_dd_nqse, 2)}")
                     
                                
 
